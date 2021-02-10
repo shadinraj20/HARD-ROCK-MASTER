0199c83449094953f4ad941b4fff61738930aa7d
shadin <shadinraj20@gmai.comecho #>
2021-02-11 01:02:05 +0600

    complite hard-rock

---
+++ app.js
@@ -0,0 +1,43 @@
+const searchSongs = () => {
+  const searchText = document.getElementById('search-field').value
+  const url = `https://api.lyrics.ovh/suggest/${searchText}`
+  fetch(url)
+    .then(res => res.json())
+    .then(data => displaySongs(data.data))
+
+}
+const displaySongs = songs => {
+  console.log(songs);
+  const songContainer = document.getElementById('song-container');
+  songContainer.innerHTML='';
+  songs.forEach(song => {
+    const songDiv = document.createElement('div');
+    songDiv.className = "single-result row align-items-center my-3 p-3";
+    songDiv.innerHTML = `
+      <div class="col-md-9">
+                        <h3 class="lyrics-name">${song.title}</h3>
+                        <p class="author lead">Album by <span>${song.artist.name}</span></p>
+                          <audio controls>
+                           <source src="${song.preview}" type="audio/ogg"> 
+                         </audio>
+                    </div>
+                    <div class="col-md-3 text-md-right text-center">
+                        <button onclick="getLyric('${song.artist.name}','${song.title}')" class="btn btn-success">Get Lyrics</button>
+                    </div> `;
+    songContainer.appendChild(songDiv);
+
+
+  })
+}
+
+     const getLyric = (artist,title) =>{
+     url =` https://api.lyrics.ovh/v1/${artist}/${title}`;
+         fetch(url)
+        .then(res => res.json())
+        .then(data => displayLyric(data.lyrics))
+      };
+
+      const displayLyric = lyrics => {
+      const lyricDiv = document.getElementById('song-lyrics');
+      lyricDiv.innerText = lyrics;
+      }
